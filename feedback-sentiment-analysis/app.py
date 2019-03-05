f444ecdff5345f9429052e1710afff1b3e480b2a
Tim Ryan <tim.ryan@ozmoapp.com>
2019-03-05 14:03:32 -0500

    Properly count lines and add another example feedback line.

--- feedback-sentiment-analysis/app.py
+++ feedback-sentiment-analysis/app.py
@@ -14,20 +14,26 @@ s3 = boto3.client('s3',
 def lambda_handler(event, context):
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf8')
+
     try:
         response = s3.get_object(Bucket=bucket, Key=key)
         lines = response['Body'].read().decode('utf8').splitlines(True)
         csv_reader = csv.DictReader(lines)
         line_count = 0
+
         for row in csv_reader:
             if line_count == 0:
                 line_count += 1
+
             feedback = TextBlob(row['feedback'])
             sentiment = feedback.sentiment
+
             if sentiment.polarity < 0:
                 print('{} from {} was NEGATIVE!'.format(row['feedback'], row['username']))
             else:
                 print('{} from {} was NEUTRAL to POSITIVE!'.format(row['feedback'], row['username']))
+
+            line_count+= 1
         print('Processed {} lines.'.format(line_count))
     except Exception as e:
         print(e)
