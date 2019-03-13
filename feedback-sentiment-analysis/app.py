b6d925a4b916862f592041fc3bfb068191e61036
Tim Ryan <tim.ryan@ozmoapp.com>
2019-03-13 16:33:56 -0400

    Simplify sentiment calculations with DataFrames.

--- feedback-sentiment-analysis/app.py
+++ feedback-sentiment-analysis/app.py
@@ -1,8 +1,7 @@
 import json
 import urllib.parse
 import boto3
-import csv
-
+import pandas as pd
 from textblob import TextBlob
 
 s3 = boto3.client('s3',
@@ -11,31 +10,30 @@ s3 = boto3.client('s3',
                     aws_access_key_id='123',
                     aws_secret_access_key='abc')
 
+def respond(err, res=None):
+    return {
+        'statusCode': '400' if err else '200',
+        'body': err.message if err else res,
+        'headers': {
+            'Content-Type': 'application/json'
+        }
+    }
+
 def lambda_handler(event, context):
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf8')
+    print(key)
 
     try:
         response = s3.get_object(Bucket=bucket, Key=key)
-        lines = response['Body'].read().decode('utf8').splitlines(True)
-        csv_reader = csv.DictReader(lines)
-        line_count = 0
+        df = pd.read_csv(response['Body'])
+        df['sentiment'] = df.apply(calculate_sentiment, axis=1)
 
-        for row in csv_reader:
-            if line_count == 0:
-                line_count += 1
-
-            feedback = TextBlob(row['feedback'])
-            sentiment = feedback.sentiment
-
-            if sentiment.polarity < 0:
-                print('{} from {} was NEGATIVE!'.format(row['feedback'], row['username']))
-            else:
-                print('{} from {} was NEUTRAL to POSITIVE!'.format(row['feedback'], row['username']))
-
-            line_count+= 1
-        print('Processed {} lines.'.format(line_count))
+        return respond(None, df.to_json)
     except Exception as e:
         print(e)
         print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
-        raise e
+
+def calculate_sentiment(row):
+    print(row)
+    return 'negative' if TextBlob(row.feedback).sentiment.polarity < 0 else 'positive'
